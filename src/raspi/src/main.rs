095bccadd3ab335844c19221ca2f7a88fbbd968b
mac-casellaj <casellaj@mcmaster.ca>
2022-11-19 21:52:51 -0500

    raspi code now reconnects to serial while running, also added dpad to frontend

--- src/raspi/src/main.rs
+++ src/raspi/src/main.rs
@@ -2,33 +2,75 @@ use futures::StreamExt;
 use serde::Serialize;
 use serialport::SerialPort;
 use tokio::sync::Mutex;
-use std::env;
 use std::sync::Arc;
 use std::time::Duration;
 use std::time::Instant;
 use warp::{reply::json, ws::WebSocket, Filter, Reply};
 
+pub struct State {
+    start_time: Instant,
+    port: Option<Box<dyn SerialPort>>,
+}
+
+impl State {
+    fn serial_write(&mut self, data: &[u8]) {
+        let port = match &mut self.port {
+            Some(port) => port,
+            None => {
+                println!("No serial port connected, listing available ports");
+                match serialport::available_ports() {
+                    Ok(available_ports) => {
+                        for available_port in available_ports {
+                            print!("Attempting to connect to {} ... ", available_port.port_name);
+
+                            let attemped_port = serialport::new(available_port.port_name, 9600)
+                                .timeout(Duration::from_millis(10))
+                                .open();
+
+                            match attemped_port {
+                                Ok(v) => {
+                                    println!("Success!");
+                                    self.port = Some(v);
+                                    break;
+                                },
+                                Err(e) => println!("Failure: {}", e),
+                            }
+                        }
+
+                        match &mut self.port {
+                            Some(port) => port,
+                            None => {
+                                println!("Failed to find a connection");
+                                return;
+                            }
+                        }
+                    },
+                    Err(e) => {
+                        println!("Error listing available ports: {}", e);
+                        return;
+                    },
+                }
+            },
+        };
+
+        if let Err(e) = port.write(data) {
+            println!("Error writing to serial, dropping connection: {}", e);
+            self.port = None;
+        }
+    } 
+}
+
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        println!("Invalid params {:#?}", args);
-        return;
-    }
+    let state = Arc::new(Mutex::new(State{
+        start_time: Instant::now(),
+        port: None,
+    }));
 
-    let start_time = Instant::now();
-    let health_route = warp::path("health").and_then(move || health_handler(start_time));
+    let health_route_state = state.clone();
+    let health_route = warp::path("health").and_then(move || health_handler(health_route_state.clone()));
 
-    // Open the serial port
-    // Defining the serial port for communication
-    let port = serialport::new(&args[1], 115_200)
-        .timeout(Duration::from_millis(10))
-        .open()
-        .expect("Failed to open port");
-
-    let shared_port = Arc::new(Mutex::new(port));
-
-    let ws_route = warp::path("ws").and(warp::ws()).and_then(move |ws| ws_handler(ws, shared_port.clone()));
+    let ws_route = warp::path("ws").and(warp::ws()).and_then(move |ws| ws_handler(ws, state.clone()));
 
     let frontend = warp::path::end().and(warp::fs::file("assets/GUI-HTML.html"));
     let assets = warp::path("assets").and(warp::fs::dir("assets"));
@@ -42,15 +84,15 @@ async fn main() {
     warp::serve(routes).run(([0, 0, 0, 0], 6060)).await;
 }
 
-pub async fn ws_handler(ws: warp::ws::Ws, shared_port: Arc<Mutex<Box<dyn SerialPort>>>) -> Result<impl Reply, warp::Rejection> {
-    Ok(ws.on_upgrade(move |socket| websocket_connection(socket, shared_port)))
+pub async fn ws_handler(ws: warp::ws::Ws, shared_state: Arc<Mutex<State>>) -> Result<impl Reply, warp::Rejection> {
+    Ok(ws.on_upgrade(move |socket| websocket_connection(socket, shared_state)))
 }
 
 pub fn make_serial_packet(left_speed: u8, right_speed: u8) -> [u8; 8] {
     return [0x10, 0x02, 0x02, left_speed, right_speed, left_speed ^ right_speed, 0x10, 0x03];
 }
 
-pub async fn websocket_connection(ws: WebSocket, shared_port: Arc<Mutex<Box<dyn SerialPort>>>) {
+pub async fn websocket_connection(ws: WebSocket, shared_state: Arc<Mutex<State>>) {
     let (mut _client_ws_sender, mut client_ws_rcv) = ws.split();
 
     while let Some(result) = client_ws_rcv.next().await {
@@ -72,7 +114,7 @@ pub async fn websocket_connection(ws: WebSocket, shared_port: Arc<Mutex<Box<dyn 
 
         println!("Got {}", msg_text);
 
-        let mut port = shared_port.lock().await;
+        let mut state = shared_state.lock().await;
         let serial_data = match msg_text {
             "FORWARD" => make_serial_packet(255, 255),
             "BACK" => make_serial_packet(127, 127),
@@ -82,7 +124,7 @@ pub async fn websocket_connection(ws: WebSocket, shared_port: Arc<Mutex<Box<dyn 
             _ => continue,
         };
 
-        let _ = port.write(&serial_data);
+        state.serial_write(&serial_data);
     }
 }
 
@@ -91,8 +133,10 @@ pub struct HealthResponse {
     uptime: String,
 }
 
-pub async fn health_handler(start_time: std::time::Instant) -> Result<impl Reply, warp::Rejection> {
+pub async fn health_handler(shared_state: Arc<Mutex<State>>) -> Result<impl Reply, warp::Rejection> {
+    let state = shared_state.lock().await;
+    
     return Ok(json(&HealthResponse {
-        uptime: format!("{} s", start_time.elapsed().as_secs_f32()),
+        uptime: format!("{} s", state.start_time.elapsed().as_secs_f32()),
     }));
 }
